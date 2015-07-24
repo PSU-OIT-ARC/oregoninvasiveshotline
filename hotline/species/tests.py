c2d9c0101995c4304176fc878c4e06a5bc55975b
Paul Thompson <pbt@pdx.edu>
2015-07-23 18:00:57 -0700

    Create and detail view tests for species, category, and severity

--- hotline/species/tests.py
+++ hotline/species/tests.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
 from model_mommy.mommy import make
 
 from .models import Category, Severity, Species
-
+from .views import SpeciesCreateView, SpeciesDeleteView, SpeciesDetailView, CategoryCreateView, CategoryDeleteView, CategoryDetailView, SeverityCreateView, SeverityDeleteView, SeverityDetailView
 
 class CategoryTest(TestCase):
     def test_str(self):
@@ -17,3 +18,102 @@ class SeverityTest(TestCase):
 class SpeciesTest(TestCase):
     def test_str(self):
         self.assertEqual(str(make(Species, name="foo", scientific_name="bar")), "foo (bar)")
+        
+#View tests
+
+class SpeciesCreateViewTest(TestCase):
+
+    def test_get_success_url(self):
+        
+        self.factory = RequestFactory()
+        request = self.factory.get(reverse("species_create"))
+        view = SpeciesCreateView()
+        view.request = request
+        full_path = view.get_success_url()
+        self.assertEqual(full_path, request.get_full_path())
+
+
+
+
+class SpeciesDetailViewTest(TestCase):
+
+    def test_get_success_url(self):
+    
+        new_species = make(Species)
+        self.factory = RequestFactory()
+        request = self.factory.get(reverse("species_detail", args=[new_species.pk]))
+        view = SpeciesDetailView()
+        view.request = request
+        full_path = view.get_success_url()
+        self.assertEqual(full_path, request.get_full_path())
+       
+
+        
+class SpeciesDeleteViewTest(TestCase):
+
+    def test_get_object(self):
+
+        test = "test"
+        
+
+class CategoryCreateViewTest(TestCase):
+
+    def test_get_success_url(self):
+        
+        self.factory = RequestFactory()
+        request = self.factory.get(reverse("category_create"))
+        view = CategoryCreateView()
+        view.request = request
+        full_path = view.get_success_url()
+        self.assertEqual(full_path, request.get_full_path())
+
+
+class CategoryDetailViewTest(TestCase):
+
+    def test_get_success_url(self):
+        
+        new_category = make(Category)
+        self.factory = RequestFactory()
+        request = self.factory.get(reverse("category_detail", args=[new_category.pk]))
+        view = CategoryDetailView()
+        view.request = request
+        full_path = view.get_success_url()
+        self.assertEqual(full_path, request.get_full_path())
+
+        
+class CategoryDeleteViewTest(TestCase):
+
+    def test_get_object(self):
+        test = "test"
+        
+
+class SeverityCreateViewTest(TestCase):
+
+    def test_get_success_url(self):
+
+        self.factory = RequestFactory()
+        request = self.factory.get(reverse("severity_create"))
+        view = SeverityCreateView()
+        view.request = request
+        full_path = view.get_success_url()
+        self.assertEqual(full_path, request.get_full_path())
+
+
+class SeverityDetailViewTest(TestCase):
+
+    def test_get_success_url(self):
+
+        new_severity = make(Severity)
+        self.factory = RequestFactory()
+        request = self.factory.get(reverse("severity_detail", args=[new_severity.pk]))
+        view = SeverityDetailView()
+        view.request = request
+        full_path = view.get_success_url()
+        self.assertEqual(full_path, request.get_full_path())
+
+        
+class SeverityDeleteViewTest(TestCase):
+
+    def test_get_object(self):
+        test = "test"
+
