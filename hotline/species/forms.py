dcfe30e63b8435e2ccb7bed0211785fdb0857129
Paul Thompson <pbt@pdx.edu>
2015-07-23 18:00:26 -0700

    Fixed import issues

--- hotline/species/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.conf import settings
-from django.forms import ModelForm
-from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
-from hotline.species.models import Species, Category, Severity
